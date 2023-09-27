5e4c38f08fe7ba26c1e030502f0e45229e3692e4
Jose F Rodrigues <josefrodrigues99@gmail.com>
2023-09-27 10:07:09 -0300

    feature: Macro ativada também pelo botão F4 e aumento do delay para evitar inconsistências

--- autoatk.py
+++ autoatk.py
@@ -1,3 +1,4 @@
+## Version 0.1.2
 import time
 import pyautogui as pg
 from pynput.keyboard import Listener
@@ -5,18 +6,18 @@ from pynput import keyboard
 
 LIST_ATTACKS = ['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10']
 
-def attack(hotkey, delay=0.025):
+def attack(hotkey, delay=0.2):
     # Start
-    print("Iniciando o combo")
     for item in hotkey:
         pg.press(f'{item}')
         time.sleep(delay)
     
 def key_code(key):
     if key == keyboard.Key.esc:
+        print("Finalizando script")
         return False
-    if key == keyboard.Key.f12:
+    if key == keyboard.Key.f12 or keyboard.Key.f4:
         attack(LIST_ATTACKS)
-        
-with Listener(on_press=key_code) as listener:
-    listener.join()
+
+with Listener(on_press=key_code) as Listener:
+    Listener.join()
